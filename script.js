63bf6e8cb06beba0c8d3cf7a257b0a011a9dd4d2
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-02-18 00:01:55 -0300

    finished calc

--- script.js
+++ script.js
@@ -10,40 +10,43 @@ function startCounting() {
 
     function increaseSeconds() {
         seconds += 1
-        if (seconds < 6) {
+        console.log(seconds)
+
+        if (seconds <= 9) {
+            secondsDisplay.innerText = `0${seconds}`
+        } else if (seconds > 9 && seconds <60) {
             secondsDisplay.innerText = seconds
-            increaseMinutes()
-            function increaseMinutes() {
-                if (seconds == 5) {
-                    minutes += 1
-                    console.log(minutes)
-                     if (minutes < 6) {
-                        minutesDisplay.innerText = minutes
-                        increaseHours()
-                        function increaseHours() {
-                            if (minutes == 5) {
-                                hours += 1
-
-                                if (hours < 6)
-                                    hoursDisplay.innerText = hours
-                            } else {
-                                hours = 0
-                                document.querySelector("div.hours").innerHTML = hours
-                            }
-                        }
-                    } else {
-                        minutes = 0
-                        document.querySelector("div.minutes").innerHTML = minutes
-                    }
-                }
-            }
-
         } else {
+            minutes += 1
             seconds = 0
+            console.log("zerou")
+            secondsDisplay.innerText = `0${seconds}`
         }
+        console.log(`minutos ${minutes}`)
+
+        if (minutes <= 9) {
+            minutesDisplay.innerText = `0${minutes}`
+        } else if (minutes > 9 && minutes <60) {
+            minutesDisplay.innerText = minutes
+        } else {
+            hours += 1
+            minutes = 0
+            console.log("zerou minutos")
+            minutesDisplay.innerText = `0${minutes}`
+        }
+
+        if (hours <= 9) {
+            hoursDisplay.innerText = `0${hours}`
+        } else if (hours > 9 && hours <60) {
+            hoursDisplay.innerText = hours
+        } else {
+            window.alert("You have reached the max time!") 
+        }
+        console.log(`hours ${hours}`)
     }
-    let counting = setInterval(increaseSeconds, 1000)
+    setInterval(increaseSeconds, 1000) 
 }
 
-//Debug:
-//1:0:5
+
+//Stop button function
+//Clear button function with SPA method (Do not refresh page)
